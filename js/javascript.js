5cfb12633a8909fe0699cfc94c9ae6b664c99678
Srijak Chakraborty <srijak.work@gmail.com>
2024-02-04 03:20:08 +0530

    asset paths fixed

--- js/javascript.js
+++ js/javascript.js
@@ -3,9 +3,9 @@ let food2s = document.getElementById('food2');
 let foods = document.getElementById('food');
 
 food1s.addEventListener('click', () => {
-    foods.style.backgroundImage = "url('food1.png')";
+    foods.style.backgroundImage = "url('../food1.png')";
 })
 
 food2s.addEventListener('click', () => {
-    foods.style.backgroundImage = "url('food.jpg')";
+    foods.style.backgroundImage = "url('../food.jpg')";
 })
