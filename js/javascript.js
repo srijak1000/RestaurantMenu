20e32291c300336e3d5e83ca0b4cc711210f12ba
Srijak Chakraborty <srijak.work@gmail.com>
2024-02-06 14:26:43 +0530

    Code rearranged

--- js/javascript.js
+++ js/javascript.js
@@ -1,45 +1,45 @@
-function myFun(){
-    let searchVal = document.getElementById("inp").value.toUpperCase() ;
+function myFun() {
+    let searchVal = document.getElementById("inp").value.toUpperCase();
     let table = document.getElementsByTagName("table")
     let tr = document.getElementsByTagName("tr");
 
     let front = document.querySelector(".main")
 
-    for(let i=0; i<tr.length; i++){
+    for (let i = 0; i < tr.length; i++) {
         let td = tr[i].getElementsByTagName("td")[0];
-        if(td){
+        if (td) {
             let text = td.innerText;
-            if(text.toLocaleUpperCase().indexOf(searchVal) > -1){
-                tr[i].style.display="";
-                front.style.display="none"
-            }else{
-                tr[i].style.display="none"
+            if (text.toLocaleUpperCase().indexOf(searchVal) > -1) {
+                tr[i].style.display = "";
+                front.style.display = "none"
+            } else {
+                tr[i].style.display = "none"
             }
         }
     }
 
-    if(searchVal == ""){
-        front.style.display="flex"
+    if (searchVal == "") {
+        front.style.display = "flex"
     }
     console.log(searchVal);
 }
 
 
 
-function nav(){
+function nav() {
     let nav = document.querySelector(".nav");
-    nav.style.display="block"
+    nav.style.display = "block"
     let login = document.querySelector(".login");
-    login.style.display="none";
+    login.style.display = "none";
 
 }
 
-function login(){
+function login() {
     let login = document.querySelector(".login");
-    login.style.display="block";
+    login.style.display = "block";
 
     let nav = document.querySelector(".nav");
-    nav.style.display="none"
+    nav.style.display = "none"
 
 }
 
@@ -50,38 +50,38 @@ let slide = document.querySelectorAll(".customer");
 let count = 0;
 console.log(slide);
 
-slide.forEach(function(customer, index){
-    customer.style.left=`${index * 100}%`
+slide.forEach(function (customer, index) {
+    customer.style.left = `${index * 100}%`
 })
 
-function next(){
-    count ++;
-    if(count == slide.length){
-        count=0;
+function next() {
+    count++;
+    if (count == slide.length) {
+        count = 0;
     }
     bar()
 }
 
-function pre(){
+function pre() {
     count--;
-    if(count == -1){
-        count = slide.length-1
+    if (count == -1) {
+        count = slide.length - 1
     }
     bar()
 }
 
 
-function bar(){
-    slide.forEach(function(customer){
+function bar() {
+    slide.forEach(function (customer) {
         customer.style.transform = `translateX(-${count * 100}%)`
     })
 }
 
 let a = {
-    greet:"HI"
+    greet: "HI"
 }
 let c;
-c=a;
-a.age=11;
+c = a;
+a.age = 11;
 
 console.log(c.age);
